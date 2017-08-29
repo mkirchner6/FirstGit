4c1db620595e1eb1bc84cedc5b65c68b61d96257
mattkirchner <mattkirchner@10.30.21.107>
2017-08-29 15:45:36 -0500

    hopeful fix

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package firstgit;
 
 /**
@@ -12,6 +17,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("second comment");
+
     }
     
 }
